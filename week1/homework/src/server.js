ab94c75ee1df6c6535798388bc0233e0d16444bc
Elia <elahehahmadiehyazdi@gmail.com>
2019-09-06 18:36:38 +0200

    first actual try!

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -6,44 +6,24 @@ const http = require('http');
 function createServer(port) {
   let state = 10;
 
-  function resWrite() {
-    // eslint-disable-next-line no-undef
-    res.write(JSON.stringify({
-      'state': state
-    }));
-  }
-
-  const server = http.createServer((req, res) => {
-    res.writeHead(200, {
-      'Content-Type': 'app/json'
+  const server = http.createServer((req, response) => {
+    response.writeHead(200, {
+      'Content-Type': 'application/json'
     });
-    switch (req.url) {
-      case '/state':
-        resWrite();
-        break;
-      case '/add':
-        state += 1;
-        resWrite();
-        break;
-      case '/subtract':
-        state -= 1;
-        resWrite();
-        break;
-      case '/reset':
-        state = 10;
-        resWrite();
-        break;
+    if (req.url == '/state') {
+      let json = JSON.stringify({
+        'state': 10
+      });
+      response.write(json);
+    }
+    if (req.url == '/add') {
+      state += 1;
+      let json = JSON.stringify({
+        'state': state
+      });
+      response.write(json);
+    }
 
-      default:
-        // eslint-disable-next-line no-undef
-        response.writeHead(404, {
-          'Content-Type': 'app/json'
-        });
-        // eslint-disable-next-line no-undef
-        response.write(JSON.stringify({
-          'error': 'Not found'
-        }));
-    };
     // eslint-disable-next-line no-undef
     response.end();
   });
