dbade0a62ee8b7dd1b16d99ea49c2372274bce79
Elia <elahehahmadiehyazdi@gmail.com>
2019-09-06 11:54:15 +0200

    can't find index module!

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -1,4 +1,4 @@
-  
+
 'use strict';
 
 const http = require('http');
@@ -6,11 +6,12 @@ const http = require('http');
 function createServer(port) {
   let state = 10;
 
-  function resWrite () { 
+  function resWrite() {
+    // eslint-disable-next-line no-undef
     res.write(JSON.stringify({
-    'state': state
-  }))
-}
+      'state': state
+    }));
+  }
 
   const server = http.createServer((req, res) => {
     res.writeHead(200, {
@@ -18,33 +19,35 @@ function createServer(port) {
     });
     switch (req.url) {
       case '/state':
-    resWrite() 
+        resWrite();
         break;
       case '/add':
         state += 1;
-       resWrite();
+        resWrite();
         break;
       case '/subtract':
         state -= 1;
-      resWrite();
+        resWrite();
         break;
       case '/reset':
         state = 10;
-      resWrite()
+        resWrite();
         break;
-      
+
       default:
+        // eslint-disable-next-line no-undef
         response.writeHead(404, {
           'Content-Type': 'app/json'
         });
+        // eslint-disable-next-line no-undef
         response.write(JSON.stringify({
           'error': 'Not found'
         }));
     };
+    // eslint-disable-next-line no-undef
     response.end();
-  }
-}
-  
+  });
+
   return server;
 }
 
