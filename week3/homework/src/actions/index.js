b8b128fcfe5c23baf6cd9d57ff77971afa678898
suthatta <psuthatta@gmail.com>
2019-10-05 21:23:59 +0200

    Delete index.js
    d

--- week3/homework/src/actions/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  createTodo: require('./createTodo'),
-  readTodo: require('./readTodo'),
-  markAsDone: require('./markAsDone'),
-  markAsNotDone: require('./markAsNotDone'),
-  clearTodo: require('./clearTodo')
-};
