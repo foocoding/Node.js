474b367f8e236bf39939481692e1c4c90ad76d57
chichiglacierz <tytrainor@hotmail.com>
2019-11-24 13:32:31 +0100

    modified week 2 code

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -1,3 +1,35 @@
 'use strict';
+const fs = require('fs');
+const yargs = require('yargs');
 
-// TODO: Write the homework code in this file
+
+const add = require('./add.js');
+const deletes = require('./deletes.js');
+const list = require('./list.js');
+const help = require('./help.js');
+const reset = require('./reset.js');
+
+// const argv = yargs.argv;
+
+let command = argv._[0];
+console.log(command);
+
+if (command === 'addTodo') {
+  add.addTodo(argv.title);
+} else if (command === 'deleteTodo') {
+  let todoDeleted = deletes.deleteTodo(argv.title);
+  let message = todoDeleted ? 'Todo was deleted' : 'Todo not found';
+  console.log(message);
+} else if (command === 'listTodos') {
+  let allTodos = list.listTodos();
+  console.log(`Printing ${allTodos.length} todo(s).`);
+  allTodos.forEach(todo => list.logTodo(todo));
+} else if (command === 'resetTodo') {
+  reset.resetTodo();
+  const message = 'All Todos have been removed';
+  console.log(message);
+} else if (command === 'printHelp') {
+  help.printHelp();
+} else {
+  console.log('Invalid command.');
+}
